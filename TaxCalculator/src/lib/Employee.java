b37e1c9f84b08b00ef07b7f15d015bff3eb0270c
aditya prabu mukti <137145289+sangprabuuu@users.noreply.github.com>
2025-04-17 17:56:41 +0700

    Refactor Duplicate Code - setMonthlySalary

--- TaxCalculator/src/lib/Employee.java
+++ TaxCalculator/src/lib/Employee.java
@@ -48,7 +48,7 @@ public class Employee {
 	}
 	
 	/**
-	 * Fungsi untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
+	 * Fungsi  untuk menentukan gaji bulanan pegawai berdasarkan grade kepegawaiannya (grade 1: 3.000.000 per bulan, grade 2: 5.000.000 per bulan, grade 3: 7.000.000 per bulan)
 	 * Jika pegawai adalah warga negara asing gaji bulanan diperbesar sebanyak 50%
 	 */
 	
